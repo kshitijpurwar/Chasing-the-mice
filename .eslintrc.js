258845743b5a113a3ba9b00a73e3eb1f98dae193
Kshitij Purwar <kshitij.purwar4@gmail.com>
2017-10-17 00:59:27 +0530

    Add ESlint to project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+    	browser : true
+    },
+    "rules" :{
+    	"indent" : ["error", "tab"],
+    	"no-console": 0,
+    	"no-tabs": 0,
+    	"no-undef": "warn"
+    }
+};
