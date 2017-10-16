4328c92be066530c0cf63e398b9f9dad77352e3b
Kshitij Purwar <kshitij.purwar4@gmail.com>
2017-10-17 00:59:55 +0530

    Fix lint errors

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-let express = require("express");
-let socket = require("socket.io");
+const express = require('express');
+const socketio = require('socket.io');
 
 // App Setup
 const app = express();
-const server = app.listen(8080, function(){
+const server = app.listen(8080, () => {
 	console.log('Listening to requests on port - 8080');
 });
 
 app.use(express.static('public'));
 
 //  Socket App
-const io = socket(server);
+const io = socketio(server);
 
 let connected = 0;
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
 	console.log('Someone connected through sockets');
-	console.log('Total Connected clients', connected++);
+	connected += 1;
+	console.log('Total Connected clients', connected);
 
-	socket.on('updatePostion', function(data){
+	socket.on('updatePostion', (data) => {
 		console.log('Someone just updated the position of arrow to', data.x, data.y);
 		socket.broadcast.emit('updatePostion', data);
 	});
 
-	socket.on('disconnect', function(){
+	socket.on('disconnect', () => {
 		console.log('Someone just left');
-		console.log('Total Connected clients', connected--);
+		connected -= 1;
+		console.log('Total Connected clients', connected);
 	});
 });
-
--- public/app.js
+++ public/app.js
@@ -3,25 +3,25 @@ const socket = io('http://localhost:8080');
 
 const arrow = document.querySelector('.arrow');
 
-function	updateArrowPosition(coordinates){
-	arrow.style.left = coordinates.x + "px";
-	arrow.style.top = coordinates.y + "px";
-};
+function updateArrowPosition(coordinates) {
+	arrow.style.left = `${coordinates.x}px`;
+	arrow.style.top = `${coordinates.y}px`;
+}
 
 document.addEventListener('mousemove', (event) => {
-	coordinates  = {
+	const coordinates = {
 		x: event.clientX,
-		y: event.clientY
+		y: event.clientY,
 	};
 	console.log('Mouse moved', coordinates);
 	updateArrowPosition(coordinates);
 
-	socket.emit('updatePostion', { x : coordinates.x , y : coordinates.y });
+	socket.emit('updatePostion', { x: coordinates.x, y: coordinates.y });
 	console.log('Message sent through websockets');
 });
 
 socket.on('updatePostion', (data) => {
 	console.log('From server', data);
 	updateArrowPosition(data);
-})
+});
 
